ffa802683b9658c74fbfe0d4b6918190aa8ff39d
allain <allaindemoupu@mail.ru>
2024-07-27 20:00:43 +0300

    Добавлены правила CORS

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 import uvicorn
 
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Заглушка БД.
 # Список тем.
